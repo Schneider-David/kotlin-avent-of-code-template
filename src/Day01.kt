a72244ba6096beea88af35c87dfc4af41dcfba63
David <david.schneider.git@gmail.com>
2022-12-02 18:06:10 -0600

    Day01 updates

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: String): List<List<Int>> {
+        val data = input.split("\r\n\r\n").map { elf ->
+            elf.lines().map { it.toInt() }
+        }
+        return data
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        //why is this coming in as \r\n...?
+        val data = parseInput(input)
+        //            elf.split("\n").map { it.toInt() } }
+
+        return data.maxOf { it.sum() }
+    }
+
+    fun part2(input: String): Int {
+        return parseInput(input).map { it -> it.sum() }.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = File("src/Day01_test.txt").readText()
+    val input = File("src/Day01.txt").readText()
 
-    val input = readInput("Day01")
-    println(part1(input))
+//    check(part1(testInput) == 24000)
+
+    println(part1(testInput))
     println(part2(input))
 }
